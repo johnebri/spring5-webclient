e947c4be95d2907f9e371531d797b3c14ded79a3
John Ebri <john.ebri@yahoo.com>
2021-06-29 13:30:59 +0000

    Added ExchangeFilters

--- src/main/java/com/johnebri/webclient/Controllers.java
+++ src/main/java/com/johnebri/webclient/Controllers.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.core.publisher.Flux;
@@ -22,12 +25,15 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api/v1")
 public class Controllers {
 
-	 WebClient webClient;
+	Logger log = LoggerFactory.getLogger(Controllers.class);
+
+	WebClient webClient;
 
 	@PostConstruct
 	public void init() {
 		webClient = WebClient.builder().baseUrl("https://api.creditRegistry.org/nigeria/AutoCred/v7.Test")
-				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).filter(logRequest())
+				.filter(logResponse()).build();
 
 	}
 
@@ -35,7 +41,7 @@ public class Controllers {
 	public Mono<String> Login(@RequestBody LoginDto loginDto) {
 		return webClient.post().uri("/api/Agents/Login").syncBody(loginDto).retrieve().bodyToMono(String.class);
 	}
-	
+
 ////	@PostMapping("/login")
 ////	public LoginDto Login(@RequestBody LoginDto loginDto) {
 ////		return loginDto;
@@ -44,17 +50,29 @@ public class Controllers {
 ////	public Flux<LoginDto> getAllLogins() {
 ////		return webClient.get().uri("/test/getall").retrieve().bodyToFlux(LoginDto.class);
 ////	}
-	
+
 //	@GetMapping("/trackBooking/{bookingId}")
 //	public Mono<BookRequest> getBookingById(@PathVariable int bookingId) {
 //		return webClient.get().uri("").retrieve().bodyToMono(BookRequest.class);
 //	}
-	
+
 //	@DeleteMapping("/deleteBooking/{bookingId}")
 //	public Mono<String> deleteBooking(int bookingId) {
 //		return webClient.delete().uri("").retrieve().bodyToMono(String.class);
 //	}
-	
-	
+
+	private ExchangeFilterFunction logRequest() {
+		return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+			log.info("Request {} {} ", clientRequest.method(), clientRequest.url());
+			return Mono.just(clientRequest);
+		});
+	}
+
+	private ExchangeFilterFunction logResponse() {
+		return ExchangeFilterFunction.ofResponseProcessor(clientResponse -> {
+			log.info("Response Status Code {} ", clientResponse.statusCode());
+			return Mono.just(clientResponse);
+		});
+	}
 
 }
